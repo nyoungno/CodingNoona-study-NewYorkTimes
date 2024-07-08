1539105041f28dd26bc63f62b1af6ce0e39167c6
nyoungno Ko <yghjbm@gmail.com>
2024-07-09 08:25:16 +0900

    api url 코드수정3

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // const API_KEY = `d87115fdbe2240cb8187821a25a1fe4b`;
 let news = [];
 const getLatestNews = async () => {
-  const url = new URL(
-    //`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
-    `https://knn-newyorktimes.netlify.app/top-headlines`
-  );
+  //   const url = new URL(
+  //     `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+  //   );
+  let url = `https://knn-newyorktimes.netlify.app/top-headlines`;
   const response = await fetch(url);
   const data = await response.json();
   news = data.articles;
